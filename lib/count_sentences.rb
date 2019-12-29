fbeba994b7017a0b74422fe93e016816438ab82d
Hannah Harrington <hannahharrington@outlook.com>
2019-12-29 23:58:20 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,23 +1,32 @@
 require 'pry'
-
 class String
 
   def sentence?
-     if sentence.self = sentence
-       puts "HI!".end_with?("!") => true
-     else false 
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
-end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
   end
 end
